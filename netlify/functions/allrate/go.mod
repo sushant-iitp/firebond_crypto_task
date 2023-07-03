583a053b9986cbc90641c6a04c7df1896d9bc588
sushant <cs46sushantiitp@gmail.com>
2023-07-03 12:18:41 +0530

    check db connetion

---
+++ netlify/functions/allrate/go.mod
@@ -0,0 +1,3 @@
+module github.com/sushant-iitp/hellogo/netlify/functions/allrate
+
+go 1.18
--- netlify/functions/hello/main.go
+++ netlify/functions/hello/main.go
@@ -1,17 +1,68 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	return &events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello, World!",
+type CryptoResponse struct {
+	Symbols []string `json:"symbols"`
+}
+
+func GetCryptoSymbols(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	dbHost := os.Getenv("DB_HOST")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUsername, dbPassword, dbHost, dbName))
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, err
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT symbol FROM Cryptocurrencies")
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, err
+	}
+	defer rows.Close()
+
+	symbols := []string{}
+	for rows.Next() {
+		var symbol string
+		if err := rows.Scan(&symbol); err != nil {
+			return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, err
+		}
+		symbols = append(symbols, symbol)
+	}
+	if err := rows.Err(); err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, err
+	}
+
+	response := CryptoResponse{
+		Symbols: symbols,
+	}
+
+	body, err := json.Marshal(response)
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       string(body),
 	}, nil
 }
 
 func main() {
-	lambda.Start(handler)
+	lambda.Start(GetCryptoSymbols)
 }
